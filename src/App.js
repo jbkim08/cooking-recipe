f8f74244a67e895d026708953d9896cc03bcc8e8
JBKIM <drv98@naver.com>
2024-06-13 14:34:09 +0900

    리액트 라우트

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/home/Home";
+import Recipe from "./pages/recipe/Recipe";
+import Search from "./pages/search/Search";
+import Create from "./pages/create/Create";
+
 import "./App.css";
 
 function App() {
-  return <div className="App">앱시작</div>;
+  //리액트 라우트는 최상단에 브라우저라우트>라우츠>라우트
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/recipes/:id" element={<Recipe />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
