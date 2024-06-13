97bf6199021074e175966a4e57c674df1ab31d17
JBKIM <drv98@naver.com>
2024-06-13 15:20:59 +0900

    홈에 레시피 불러오기

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,15 @@
+import { useFetch } from "../../hooks/useFetch";
 import "./Home.css";
 
 const Home = () => {
-  return <div>홈</div>;
+  const { data, isPending, error } = useFetch("http://localhost:3030/recipes");
+  return (
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">로딩중...</p>}
+      {data && data.map((recipe) => <h2 key={recipe.id}>{recipe.title}</h2>)}
+    </div>
+  );
 };
 
 export default Home;
