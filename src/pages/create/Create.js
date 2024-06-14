9b523cd7a094dfe9989b803c89db00b02b0d6fdd
JBKIM <drv98@naver.com>
2024-06-14 10:16:01 +0900

    요리재료추가하기

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./Create.css";
 
 const Create = () => {
@@ -6,9 +6,22 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [method, setMethod] = useState("");
   const [cookingTime, setCookingTime] = useState("");
+  //요리재료와 요리재료들 배열 추가
+  const [newIngredient, setNewIngredient] = useState("");
+  const [ingredients, setIngredients] = useState([]);
+  const ingredientInput = useRef(); //리액트에서 선택방법
   const handleSubmit = (e) => {
     e.preventDefault(); //기존 이벤트 중지
     console.log(title, method, cookingTime);
+  };
+  const handleAdd = (e) => {
+    e.preventDefault(); //기존 이벤트 중지
+    const ing = newIngredient.trim(); //공백 제거
+    if (ing && !ingredients.includes(ing)) {
+      setIngredients((prev) => [...prev, ing]);
+    }
+    setNewIngredient("");
+    ingredientInput.current.focus(); //커서를 입력창에 위치
   };
   return (
     <div className="create">
@@ -25,6 +38,26 @@ const Create = () => {
         </label>
 
         {/* recipe ingredients here */}
+        <label>
+          <span>요리 재료:</span>
+          <div className="ingredients">
+            <input
+              type="text"
+              onChange={(e) => setNewIngredient(e.target.value)}
+              value={newIngredient}
+              ref={ingredientInput}
+            />
+            <button onClick={handleAdd} className="btn">
+              사용
+            </button>
+          </div>
+        </label>
+        <p>
+          재료들 :{" "}
+          {ingredients.map((i) => (
+            <em key={i}>{i}, </em>
+          ))}
+        </p>
 
         <label>
           <span>요리 방법:</span>
