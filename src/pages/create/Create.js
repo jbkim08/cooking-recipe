8e9efd796e4faa8574acc116b828e836b816f0d0
JBKIM <drv98@naver.com>
2024-06-14 09:42:16 +0900

    새 요리만들기1

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,7 +1,54 @@
+import { useState } from "react";
 import "./Create.css";
 
 const Create = () => {
-  return <div>크레이트</div>;
+  //새 레시피 작성시 제목, 요리방법, 요리시간
+  const [title, setTitle] = useState("");
+  const [method, setMethod] = useState("");
+  const [cookingTime, setCookingTime] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault(); //기존 이벤트 중지
+    console.log(title, method, cookingTime);
+  };
+  return (
+    <div className="create">
+      <h2 className="page-title">새 레시피를 추가하세요</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>요리 제목:</span>
+          <input
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+          />
+        </label>
+
+        {/* recipe ingredients here */}
+
+        <label>
+          <span>요리 방법:</span>
+          <textarea
+            onChange={(e) => setMethod(e.target.value)}
+            value={method}
+            required
+          />
+        </label>
+
+        <label>
+          <span>쿠킹 타임 (분):</span>
+          <input
+            type="number"
+            onChange={(e) => setCookingTime(e.target.value)}
+            value={cookingTime}
+            required
+          />
+        </label>
+
+        <button className="btn">추가</button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
